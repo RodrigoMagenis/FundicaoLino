eadcf8f782414abdf874f59884f25f1719f7ee1d
MasTerTGO <31973870+MasTerTGO@users.noreply.github.com>
2018-05-15 22:00:43 -0300

    fudeu

---
+++ FundicaoLino/Fundicaolino.telas/GnGridGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundicaolino.telas
+{
+    public partial class GnGridGroup : Form
+    {
+        public GnGridGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
