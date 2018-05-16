2219a6eafb1fab21a2bf3ac8cd746f190f11f425
RodrigoMagenis <rodrigolucasmagenis@gmail.com>
2018-05-16 18:51:40 -0300

    banco

---
+++ FundicaoLino/Fundicaolino.negocio/Models/TipoProduto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundicaolino.negocio.Models
+{
+    class TipoProduto
+    {
+        private long cdTipoProduto { get; set; }
+        private string nmTipoProduto { get; set; }
+        private decimal vlPeso { get; set; }
+        private long cdMateriais { get; set; }
+    }
+
+}
